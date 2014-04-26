f25b3f5b2df2a754b836045faba07c0a7c974f9a
yodi <yodi@polatic.com>
2014-04-27 05:01:38 +0700

    remove unused libraries

--- src/com/polatic/pantaudki/utils/Constant.java
+++ src/com/polatic/pantaudki/utils/Constant.java
@@ -54,12 +54,6 @@ public class Constant {
     public static final String JSON_PERCENT_REALIZATION = "persenRealisasi";
     public static final String JSON_ID = "id";
         
-    public static final String[] FACEBOOK_PERMISSION = {
-            "email",
-            "user_about_me", "user_birthday", "user_education_history",
-            "user_location", "user_work_history", "friends_birthday"
-    };
-
     public static final String API_CACHE_PROGRAM = "apicacheprogram";
     public static final String API_CACHE_URUSAN = "apicacheurusan";
     public static final String API_CACHE_SKPD= "apicacheskpd";
@@ -67,57 +61,5 @@ public class Constant {
     public static final String PARAM_NAME = "name";
     public static final String PARAM_VALUE = "value";
 
-    public static final String FILE_PATH_IMAGE = Environment.getExternalStorageDirectory()
-            .getAbsolutePath() + "/" + TAG;
-
-    public static final String CHAT_DATE_FORMAT = "yyyy-MM-dd hh:mm:ss";
-    public static final String DATE_FORMAT_DDMMYYYY = "dd-MM-yyyy";
-    public static final String DATE_FORMAT_MMDDYYYY = "MM-dd-yyyy";
-    /**
-     * Intent used to display a status message in the screen.
-     */
-    public static final String DISPLAY_STATUS_ACTION =
-            "com.android.polatic.custoom.DISPLAY_STATUS";
-
-    /**
-     * Intent used to display a chat message in the screen.
-     */
-    public static final String DISPLAY_MESSAGE_ACTION =
-            "com.android.polatic.custoom.DISPLAY_MESSAGE";
-
-    /**
-     * Intent's extra that contains the message to be displayed.
-     */
-    public static final String EXTRA_MESSAGE = "message";
-
-    /**
-     * Notifies UI to display a status message.
-     * <p>
-     * This method is defined in the common helper because it's used both by the
-     * UI and the background service.
-     * 
-     * @param context application's context.
-     * @param message message to be displayed.
-     */
-    public static void displayMessage(Context context, String message) {
-        Intent intent = new Intent(DISPLAY_STATUS_ACTION);
-        intent.putExtra(EXTRA_MESSAGE, message);
-        context.sendBroadcast(intent);
-    }
-
-    /**
-     * Notifies UI to display a chat message.
-     * <p>
-     * This method is defined in the common helper because it's used both by the
-     * UI and the background service.
-     * 
-     * @param context application's context.
-     * @param message message to be displayed.
-     */
-    public static void displayChat(Context context, String message) {
-        Intent intent = new Intent(DISPLAY_MESSAGE_ACTION);
-        intent.putExtra(EXTRA_MESSAGE, message);
-        context.sendBroadcast(intent);
-    }
 
 }
